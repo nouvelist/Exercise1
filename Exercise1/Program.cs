0163f07e3e9f2107f010ed7779e72a391f5d0326
grace <grace@CHIAKI-LIV3S-HE>
2022-03-15 17:14:24 +1300

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿string name, petName, favColor;
+int age;
+
+Console.WriteLine("Enter your name: ");
+name = Console.ReadLine();
+Console.WriteLine($"Your name is {name}!");
+
+Console.WriteLine("Enter your age: ");
+age = Console.ReadLine();
+Console.WriteLine($"Your age is {age}!");
+
+Console.WriteLine("Enter your pet's name:");
+petName = Console.ReadLine();
+Console.WriteLine($"Your pet's name is {petName}!");
+
+Console.WriteLine("Enter your favorite color: ");
+favColor = Console.ReadLine();
+Console.WriteLine($"Your favorite color is {favColor}!");
